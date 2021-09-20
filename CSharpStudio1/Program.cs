07694db8863c2dad0b48a31b0ed640e655898376
erbuckner <erbuckner@gmail.com>
2021-09-20 19:41:52 -0400

    Added files and completed first part of the studio

---
+++ CSharpStudio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpStudio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius: ");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(radius, 2);
+            area = Math.Round(area, 2);
+
+            Console.WriteLine($"The area of a circle of radius {radius} is: {area}");
+
+        }
+    }
+}
